7ee12ac312468267c440c336f7ae10b1e6a3cf19
Maggie <maggie.r.martin1@gmail.com>
2026-02-01 01:00:43 +0100

    Init next app

---
+++ app/api/wallpaper/route.ts
@@ -0,0 +1,17 @@
+const IMAGES: string[] = [
+  "https://img.freepik.com/free-vector/colorful-creepy-creatures-illustration-background_516247-1.jpg?w=2000",
+  "https://picography.co/wp-content/uploads/2024/06/picography-bird-street-art-768x528.jpg",
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Fronalpstock_big.jpg/2048px-Fronalpstock_big.jpg",
+  "https://img.freepik.com/free-photo/aesthetic-retro-vaporwave-landscape_23-2148949194.jpg?w=2000"
+];
+
+export async function GET(): Promise<Response> {
+  const image = IMAGES[Math.floor(Math.random() * IMAGES.length)];
+  return new Response(null, {
+    status: 302,
+    headers: {
+      Location: image,
+      "Cache-Control": "no-store",
+    },
+  });
+}
